c8c0cd0b0bd4b4ec372ce2581aba7ad8348fa48b
Liviu Ionescu <ilg@livius.net>
2024-11-21 11:19:13 +0200

    update copyright notice

--- dev-scripts/convert-arm-asm-to-c.sh
+++ dev-scripts/convert-arm-asm-to-c.sh
@@ -61,7 +61,7 @@ cat <<__EOF__
 /*
  * This file is part of the µOS++ distribution.
  *   (https://github.com/micro-os-plus/)
- * Copyright (c) ${current_year} Liviu Ionescu.
+ * Copyright (c) ${current_year} Liviu Ionescu. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software
  * for any purpose is hereby granted, under the terms of the MIT license.
