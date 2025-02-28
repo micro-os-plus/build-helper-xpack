25da2682510947ea778a7ca6230e55becd7697ac
Liviu Ionescu <ilg@livius.net>
2025-02-28 18:30:24 +0200

    add templates & generate-commons.sh

---
+++ maintenance-scripts/generate-commons.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+#
+# This file is part of the xPack project (http://xpack.github.io).
+# Copyright (c) 2024 Liviu Ionescu.  All rights reserved.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose is hereby granted, under the terms of the MIT license.
+#
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/mit/.
+#
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Identify the script location, to reach, for example, the helper scripts.
+
+script_path="$0"
+if [[ "${script_path}" != /* ]]
+then
+  # Make relative path absolute.
+  script_path="$(pwd)/$0"
+fi
+
+export script_path
+export script_name="$(basename "${script_path}")"
+
+export script_folder_path="$(dirname "${script_path}")"
+export script_folder_name="$(basename "${script_folder_path}")"
+
+# =============================================================================
+
+# set -x
+
+argv="$@"
+
+# Runs as
+# .../xpacks/@micros-os-plus/build-helper/maintenance-scripts/generate-commons.sh
+npm_helper_folder_path="$(dirname $(dirname $(dirname $(dirname "${script_folder_path}"))))/node_modules/@xpack/npm-packages-helper"
+
+source "${npm_helper_folder_path}/maintenance-scripts/scripts-helper-source.sh"
+
+# Parse --init, --dry-run, --xpack, --xpack-dev-tools
+# and leave variables in the environment.
+parse_options "$@"
+
+# -----------------------------------------------------------------------------
+
+if [ "${is_micro_os_plus}" != "true" ]
+then
+  echo "Unsupported configuration..."
+  exit 1
+fi
+
+current_folder_path="$(dirname $(dirname $(dirname $(dirname "${script_folder_path}"))))"
+project_folder_path="$(dirname "${current_folder_path}")"
+templates_folder_path="$(dirname "${script_folder_path}")/templates"
+
+export current_folder_path
+export project_folder_path
+export templates_folder_path
+
+# -----------------------------------------------------------------------------
+
+# Used to enforce an exit code of 255, required by xargs.
+trap 'trap_handler "${script_name}" $LINENO $?; return 255' ERR
+
+# -----------------------------------------------------------------------------
+
+# Process package.json files and leave results in environment variables.
+compute_context
+
+# -----------------------------------------------------------------------------
+
+if [ "${do_dry_run}" == "true" ]
+then
+  echo
+  echo "Dry run!"
+  echo
+fi
+
+# -----------------------------------------------------------------------------
+
+if [ "${do_init}" == "true" ]
+then
+  # TODO
+  echo "--init not implemented yet"
+  exit 1
+else
+
+  if false
+  then
+    (
+      echo
+      echo "Relocating files..."
+
+      cd "${project_folder_path}/tests"
+
+      chmod -R +w *
+
+      # relocate "..." "..."
+
+      find . -name '*.bak' -exec rm -v '{}' ';'
+    )
+  fi
+
+  echo
+  echo "Processing template from ${templates_folder_path}..."
+
+  cd "${templates_folder_path}/common"
+
+  # Main pass to copy/generate common files.
+  find . -type f -print0 | sort -zn | \
+    xargs -0 -I '{}' bash "${script_folder_path}/process-template-item.sh" --force '{}' "${project_folder_path}"
+
+  echo
+  echo "First time proposals..."
+
+  cd "${templates_folder_path}/first-time"
+
+  find . -type f -print0 | sort -zn | \
+    xargs -0 -I '{}' bash "${script_folder_path}/process-template-item.sh" '{}' "${project_folder_path}"
+
+fi
+
+# -----------------------------------------------------------------------------
+
+echo
+echo "'${script_name} ${argv}' done"
+
+# Completed successfully.
+exit 0
+
+# -----------------------------------------------------------------------------
---
+++ maintenance-scripts/process-template-item.sh
@@ -0,0 +1,159 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+#
+# This file is part of the xPack project (http://xpack.github.io).
+# Copyright (c) 2024 Liviu Ionescu.  All rights reserved.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose is hereby granted, under the terms of the MIT license.
+#
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/mit/.
+#
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Identify the script location, to reach, for example, the helper scripts.
+
+script_path="$0"
+if [[ "${script_path}" != /* ]]
+then
+  # Make relative path absolute.
+  script_path="$(pwd)/$0"
+fi
+
+export script_path
+export script_name="$(basename "${script_path}")"
+
+export script_folder_path="$(dirname "${script_path}")"
+export script_folder_name="$(basename "${script_folder_path}")"
+
+# =============================================================================
+
+# set -x
+
+# The script is invoked from xpacks/@micro-os-plus/build-helper/maintenance-scripts/generate-commons.sh.
+
+npm_helper_folder_path="$(dirname $(dirname $(dirname $(dirname "${script_folder_path}"))))/node_modules/@xpack/npm-packages-helper"
+
+source "${npm_helper_folder_path}/maintenance-scripts/scripts-helper-source.sh"
+
+# -----------------------------------------------------------------------------
+
+# Runs in the source folder.
+# $1 = relative file path to source, starts with `./`
+# $2 = absolute folder path to destination
+
+# echo "argv: $@"
+# echo "pwd: $(pwd)"
+# set -x
+
+do_force="false"
+
+if [ "${1}" == "--force" ]
+then
+  # Ask to override and write protect.
+  do_force="true"
+  shift
+fi
+
+export do_force
+
+# -----------------------------------------------------------------------------
+
+# If one of the selector paths is present, but not the right one, exit.
+if check_if_should_ignore_path "$@"
+then
+  exit 0
+fi
+
+prepare_paths "$@"
+
+# -----------------------------------------------------------------------------
+
+tmp_file_path="$(mktemp -t top_commons.XXXXX)"
+
+# Used to enforce an exit code of 255, required by xargs.
+trap 'trap_handler ${from_relative_file_path} $LINENO $? ${tmp_file_path}; return 255' ERR
+
+# -----------------------------------------------------------------------------
+
+# Superfluous, `find -type f` should not allow this.
+if [ ! -f "${from_relative_file_path}" ]
+then
+  echo "${from_relative_file_path} not a file"
+  exit 1
+fi
+
+if [ "$(basename "${from_relative_file_path}")" == ".DS_Store" ]
+then
+  echo "${from_relative_file_path} ignored" # Skip macOS specifics.
+  exit 0
+fi
+
+if [ -f "${to_absolute_file_path}" ] && [ "${do_force}" != "true" ]
+then
+  echo "already present: ${to_relative_file_path}"
+  exit 0
+fi
+
+# -----------------------------------------------------------------------------
+# Compute exclusions.
+
+# Destination relative file paths to skip.
+skip_pages_array=("BEGIN")
+
+if [ "${is_micro_os_plus}" == "true" ]
+then
+  : # if needed
+fi
+
+# -----------------------------------------------------------------------------
+
+skip_pages_array+=("END")
+
+# The array members are concatenated with spaces separators.
+# echo "skip_pages_array=${skip_pages_array[@]}"
+# echo "to_relative_file_path=${to_relative_file_path}"
+
+set +o nounset # Do not exit if variable not set (empty skip_pages_array).
+
+# To ensure proper match, use explicit spaces.
+if [[ ${skip_pages_array[@]} =~ " ${to_relative_file_path} " ]]
+then
+  # echo "skipped: ${from_relative_file_path}"
+  echo "skipped: ${to_relative_file_path}"
+  rm -f "${tmp_file_path}"
+  exit 0
+fi
+
+set -o nounset # Exit if variable not set.
+
+# -----------------------------------------------------------------------------
+
+process_file
+
+rm -rf "${tmp_file_path}"
+
+# Completed successfully.
+exit 0
+
+# -----------------------------------------------------------------------------
