7050d5188a9f4c95eba4c9c73411df7c9d17751f
Liviu Ionescu <ilg@livius.net>
2026-05-17 16:46:43 +0300

    platform object library cleanups

--- templates/common/_micro-os-plus/tests/platforms/qemu-cortex-a15/cmake/platform-library-liquid.cmake
+++ templates/common/_micro-os-plus/tests/platforms/qemu-cortex-a15/cmake/platform-library-liquid.cmake
@@ -78,16 +78,7 @@ set (
 target_compile_options (
   platform-qemu-cortex-a15-interface INTERFACE ${xpack_platform_common_args}
 )
-{% if packageConfig.hasObjectLibrary == "true" %}
-# The OBJECTS are compiled before the platform library, so they need to get the
-# same compile options.
-target_compile_options (
-  {{packageScope}}-{{packageName}}-objects
-  PRIVATE
-    $<TARGET_PROPERTY:micro-os-plus-common-options-interface,INTERFACE_COMPILE_OPTIONS>
-    ${xpack_platform_common_args}
-)
-{% endif %}
+
 # When `-flto` is used, the compile options must be passed to the linker too.
 target_link_options (
   platform-qemu-cortex-a15-interface
--- templates/common/_micro-os-plus/tests/platforms/qemu-riscv-rv32imac/cmake/platform-library-liquid.cmake
+++ templates/common/_micro-os-plus/tests/platforms/qemu-riscv-rv32imac/cmake/platform-library-liquid.cmake
@@ -86,16 +86,7 @@ target_compile_options (
   platform-qemu-riscv-rv32imac-interface
   INTERFACE ${xpack_platform_common_args}
 )
-{% if packageConfig.hasObjectLibrary == "true" %}
-# The OBJECTS are compiled before the platform library, so they need to get the
-# same compile options.
-target_compile_options (
-  {{packageScope}}-{{packageName}}-objects
-  PRIVATE
-    $<TARGET_PROPERTY:micro-os-plus-common-options-interface,INTERFACE_COMPILE_OPTIONS>
-    ${xpack_platform_common_args}
-)
-{% endif %}
+
 # When `-flto` is used, the compile options must be passed to the linker too.
 target_link_options (
   platform-qemu-riscv-rv32imac-interface
